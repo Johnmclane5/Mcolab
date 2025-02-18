363642de8596a6e7f5d7149c392eb01f8254cb80
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2025-02-18 17:18:42 +0000

    typo

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -89,7 +89,7 @@ class Mirror(TaskListener):
             "-hl": False,
             "-bt": False,
             "-ut": False,
-            "mv": False,
+            "-mv": False,
             "-i": 0,
             "-sp": 0,
             "link": "",
