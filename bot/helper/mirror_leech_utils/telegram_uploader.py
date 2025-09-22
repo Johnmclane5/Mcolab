a0cbd9b99f119e431e8d6972feecfee0829e9b6c
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2025-09-22 06:46:49 +0000

    rm space after lprefix

--- bot/helper/mirror_leech_utils/telegram_uploader.py
+++ bot/helper/mirror_leech_utils/telegram_uploader.py
@@ -163,7 +163,7 @@ class TelegramUploader:
 
     async def _prepare_file(self, file_, dirpath):
         if self._lprefix:
-            cap_mono = f"{self._lprefix} <b>{file_}</b>"
+            cap_mono = f"{self._lprefix}<b>{file_}</b>"
             self._lprefix = re_sub("<.*?>", "", self._lprefix)
             new_path = ospath.join(dirpath, f"{self._lprefix} {file_}")
             await rename(self._up_path, new_path)
